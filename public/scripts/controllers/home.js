6c847e4f5e12fc0213df125f67e254dfb5f3a368
Gabriel Biz <gabrielbiz@outlook.com>
2016-04-21 16:39:37 -0300

    Mostrar pontuacao da instituicao com estrelas

--- public/scripts/controllers/home.js
+++ public/scripts/controllers/home.js
@@ -9,5 +9,32 @@
         var vm = this;
 
         vm.stations = StationSvc.getAll();
+        vm.starsAverage = _starsAverage;
+
+        function _starsAverage(score) {
+            if (score > 4.5) {
+                return 5;
+            } else if (score > 4) {
+                return 4.5;
+            } else if (score > 3.5) {
+                return 4;
+            } else if (score > 3) {
+                return 3.5
+            } else if (score > 2.5) {
+                return 3;
+            } else if (score > 2) {
+                return 2.5
+            } else if (score > 1.5) {
+                return 2;
+            } else if (score > 1) {
+                return 1.5
+            } else if (score > 0.5) {
+                return 1;
+            } else if (score > 0) {
+                return 0.5;
+            } else {
+                return 0;
+            }
+        }
     }
 }());
--- public/scripts/services/station.js
+++ public/scripts/services/station.js
@@ -16,57 +16,57 @@
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 5
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 4.5
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 4
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 3.5
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 3
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 2.5
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 2
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 1.5
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 1
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 0.5
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
                 address: 'Rua Rio Branco, nº 797',
-                scoreAverage: 45
+                scoreAverage: 0
             }, {
                 name: 'Unimed Blumenau',
                 image: 'http://www.unimed.coop.br/portal/conteudo/materias/1256325918313fachada.jpg',
