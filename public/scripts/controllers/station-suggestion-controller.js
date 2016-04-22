bd60bf25ce176a6bf6b01dcf338828931802906d
Daniel Pamplona Soares <Daniel Pamplona Soares>
2016-04-22 11:55:33 -0300

    Início das implementações para sugerir uma nova instituição.

---
+++ public/scripts/controllers/station-suggestion-controller.js
@@ -0,0 +1,50 @@
+;
+(function() {
+    'use-strict';
+
+    angular.module('dangari-healthy').controller('StationSuggestionController', StationSuggestionController);
+
+    function StationSuggestionController($scope) {
+        var self = this;
+        console.log('carregou!!!');
+
+
+        function onLoadBufferImagem(file){
+            var uintArray = new Uint8Array(file.target.result);
+            var byteArray = [];
+            uintArray.forEach(function(val) {
+                byteArray.push(val);
+            });
+            return byteArray;
+        };
+
+        function loadImage(file, onLoadUrlImagem, onLoadBufferImagem) {
+            // Gera uma URL para ser utilizada no img do form
+            var reader = new FileReader();
+            reader.onload = onLoadUrlImagem;
+            reader.readAsDataURL(file);
+
+            // Obtém os bytes da imagem para salvar no backend
+            reader = new FileReader();
+            reader.onload = onLoadBufferImagem;
+            reader.readAsArrayBuffer(file);
+        };
+
+
+        self.loadImage = function (file) {
+            loadImage(file, self.onLoadUrlImagem, self.onLoadBufferImagem);
+        };
+
+        self.onLoadUrlImagem = function (file) {
+            self.urlImagem = file.target.result;
+            $scope.$apply();
+        };
+
+        self.onLoadBufferImagem = function (file) {
+            self.station.image = {
+                bytes: onLoadBufferImagem(file)
+            };
+        };
+
+    }
+}());
