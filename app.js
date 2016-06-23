e04972312fb00c3e06bf9e811ca59ac5c266c974
Ricardo Reiter <ricardof.reiter@gmail.com>
2016-06-23 14:21:10 -0300

    Aumentado limite de upload dos arquivos

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.engine('html', require('ejs').renderFile);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(function(req, res, next) {
 res.setHeader('Access-Control-Allow-Origin', '*');
